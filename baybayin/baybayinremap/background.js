db4555125b3c6adb4b7231bef7f75b72c32cf3ad
Alex Ibrado <alex@ibrado.org>
2015-02-23 10:13:52 +0800

    Remove multiple-character mappings for now

--- baybayin/baybayinremap/background.js
+++ baybayin/baybayinremap/background.js
@@ -22,7 +22,7 @@ var shiftState = Shift.PLAIN;
 var lastRemappedKeyEvent;
 
 var lut = {
-"Backquote": { "plain": {"plain": "`", "shifted": "ᜈ᜔ᜌ"}, "alternate": {"plain": "", "shifted":""}, "code": "Backquote"},
+"Backquote": { "plain": {"plain": "`", "shifted": "~"}, "alternate": {"plain": "", "shifted":""}, "code": "Backquote"},
 "Digit1": { "plain": {"plain": "1", "shifted": "!"}, "alternate": {"plain": "", "shifted":""}, "code": "Digit1"},
 "Digit2": { "plain": {"plain": "2", "shifted": "@"}, "alternate": {"plain": "", "shifted":""}, "code": "Digit2"},
 "Digit3": { "plain": {"plain": "3", "shifted": "#"}, "alternate": {"plain": "", "shifted":""}, "code": "Digit3"},
@@ -35,7 +35,7 @@ var lut = {
 "Digit0": { "plain": {"plain": "0", "shifted": ")"}, "alternate": {"plain": "", "shifted":""}, "code": "Digit0"},
 "Minus": { "plain": {"plain": "-", "shifted": "_"}, "alternate": {"plain": "", "shifted":""}, "code": "Minus"},
 "Equal": { "plain": {"plain": "᜔", "shifted": "+"}, "alternate": {"plain": "", "shifted":""}, "code": "Equal"},
-"KeyQ": { "plain": {"plain": "ᜃᜓᜏ", "shifted": "ᜃ᜔ᜏ"}, "alternate": {"plain": "", "shifted":""}, "code": "KeyQ"},
+"KeyQ": { "plain": {"plain": "q", "shifted": "Q"}, "alternate": {"plain": "", "shifted":""}, "code": "KeyQ"},
 "KeyW": { "plain": {"plain": "ᜏ", "shifted": "ᜏ"}, "alternate": {"plain": "", "shifted":""}, "code": "KeyW"},
 "KeyE": { "plain": {"plain": "ᜒ", "shifted": "ᜁ"}, "alternate": {"plain": "", "shifted":""}, "code": "KeyE"},
 "KeyR": { "plain": {"plain": "ᜍ", "shifted": "ᜍ"}, "alternate": {"plain": "", "shifted":""}, "code": "KeyR"},
@@ -47,24 +47,24 @@ var lut = {
 "KeyP": { "plain": {"plain": "ᜉ", "shifted": "ᜉ"}, "alternate": {"plain": "", "shifted":""}, "code": "KeyP"},
 "BracketLeft": { "plain": {"plain": "[", "shifted": "{"}, "alternate": {"plain": "", "shifted":""}, "code": "BracketLeft"},
 "BracketRight": { "plain": {"plain": "]", "shifted": "}"}, "alternate": {"plain": "", "shifted":""}, "code": "BracketRight"},
-"KeyA": { "plain": {"plain": "", "shifted": "ᜀ"}, "alternate": {"plain": "", "shifted":""}, "code": "KeyA"},
-"KeyS": { "plain": {"plain": "ᜐ", "shifted": "ᜐ᜔ᜌ"}, "alternate": {"plain": "", "shifted":""}, "code": "KeyS"},
+"KeyA": { "plain": {"plain": "a", "shifted": "ᜀ"}, "alternate": {"plain": "", "shifted":""}, "code": "KeyA"},
+"KeyS": { "plain": {"plain": "ᜐ", "shifted": "ᜐ"}, "alternate": {"plain": "", "shifted":""}, "code": "KeyS"},
 "KeyD": { "plain": {"plain": "ᜇ", "shifted": "ᜇ"}, "alternate": {"plain": "", "shifted":""}, "code": "KeyD"},
 "KeyF": { "plain": {"plain": "ᜉ", "shifted": "ᜉ"}, "alternate": {"plain": "", "shifted":""}, "code": "KeyF"},
 "KeyG": { "plain": {"plain": "ᜄ", "shifted": "ᜄ"}, "alternate": {"plain": "", "shifted":""}, "code": "KeyG"},
 "KeyH": { "plain": {"plain": "ᜑ", "shifted": "ᜑ"}, "alternate": {"plain": "", "shifted":""}, "code": "KeyH"},
-"KeyJ": { "plain": {"plain": "ᜇᜒᜌ", "shifted": "ᜇ᜔ᜌ"}, "alternate": {"plain": "", "shifted":""}, "code": "KeyJ"},
+"KeyJ": { "plain": {"plain": "j", "shifted": "J"}, "alternate": {"plain": "", "shifted":""}, "code": "KeyJ"},
 "KeyK": { "plain": {"plain": "ᜃ", "shifted": "ᜃ"}, "alternate": {"plain": "", "shifted":""}, "code": "KeyK"},
 "KeyL": { "plain": {"plain": "ᜎ", "shifted": "ᜎ"}, "alternate": {"plain": "", "shifted":""}, "code": "KeyL"},
 "Semicolon": { "plain": {"plain": ";", "shifted": ":"}, "alternate": {"plain": "", "shifted":""}, "code": "Semicolon"},
 "Quote": { "plain": {"plain": "’", "shifted": "\""}, "alternate": {"plain": "", "shifted":""}, "code": "Quote"},
 "KeyZ": { "plain": {"plain": "ᜐ", "shifted": "ᜐ"}, "alternate": {"plain": "", "shifted":""}, "code": "KeyZ"},
-"KeyX": { "plain": {"plain": "ᜃ᜔ᜐ", "shifted": "ᜃ᜔ᜐ"}, "alternate": {"plain": "", "shifted":""}, "code": "KeyX"},
-"KeyC": { "plain": {"plain": "ᜃ", "shifted": "ᜆ᜔ᜐ"}, "alternate": {"plain": "", "shifted":""}, "code": "KeyC"},
+"KeyX": { "plain": {"plain": "x", "shifted": "X"}, "alternate": {"plain": "", "shifted":""}, "code": "KeyX"},
+"KeyC": { "plain": {"plain": "c", "shifted": "C"}, "alternate": {"plain": "", "shifted":""}, "code": "KeyC"},
 "KeyV": { "plain": {"plain": "ᜊ", "shifted": "ᜊ"}, "alternate": {"plain": "", "shifted":""}, "code": "KeyV"},
 "KeyB": { "plain": {"plain": "ᜊ", "shifted": "ᜊ"}, "alternate": {"plain": "", "shifted":""}, "code": "KeyB"},
-"KeyN": { "plain": {"plain": "ᜈ", "shifted": "ᜅ"}, "alternate": {"plain": "–", "shifted":""}, "code": "KeyN"},
-"KeyM": { "plain": {"plain": "ᜋ", "shifted": "ᜋ"}, "alternate": {"plain": "—", "shifted":""}, "code": "KeyM"},
+"KeyN": { "plain": {"plain": "ᜈ", "shifted": "ᜅ"}, "alternate": {"plain": "", "shifted":""}, "code": "KeyN"},
+"KeyM": { "plain": {"plain": "ᜋ", "shifted": "ᜋ"}, "alternate": {"plain": "", "shifted":""}, "code": "KeyM"},
 "Comma": { "plain": {"plain": "᜵", "shifted": "<"}, "alternate": {"plain": "", "shifted":""}, "code": "Comma"},
 "Period": { "plain": {"plain": "᜶", "shifted": ">"}, "alternate": {"plain": "", "shifted":""}, "code": "Period"},
 "Slash": { "plain": {"plain": "᜴", "shifted": "?"}, "alternate": {"plain": "", "shifted":""}, "code": "Slash"},
@@ -98,6 +98,11 @@ function isRemappedEvent(keyData) {
          ); // requestID would be different so we are not checking for it
 }
 
+function sendOneKey(keyData, remappedKeyData) {
+  var handled;
+
+   return handled;
+}
 
 chrome.input.ime.onKeyEvent.addListener(
     function(engineID, keyData) {
@@ -115,15 +120,14 @@ chrome.input.ime.onKeyEvent.addListener(
           var remappedKeyData = keyData;
           remappedKeyData.key = lut[keyData.code][altGrState][shiftState];
           remappedKeyData.code = lut[keyData.code].code;
-        
-        if (chrome.input.ime.sendKeyEvents !== undefined) {
-          chrome.input.ime.sendKeyEvents({"contextID": contextID, "keyData": [remappedKeyData]});
-          handled = true;
-          lastRemappedKeyEvent = remappedKeyData;
-        } else if (keyData.type == "keydown" && !isPureModifier(keyData)) {
-          chrome.input.ime.commitText({"contextID": contextID, "text": remappedKeyData.key});
-          handled = true;
-        }
+          if (chrome.input.ime.sendKeyEvents !== undefined) {
+            chrome.input.ime.sendKeyEvents({"contextID": contextID, "keyData": [remappedKeyData]});
+            handled = true;
+            lastRemappedKeyEvent = remappedKeyData;
+          } else if (keyData.type == "keydown" && !isPureModifier(keyData)) {
+            chrome.input.ime.commitText({"contextID": contextID, "text": remappedKeyData.key});
+            handled = true;
+          }
       }
       
       return handled;
